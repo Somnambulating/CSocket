a78a34c65c6acb7a7293ecd30bb586d30bc549b9
Oliver <1366033875@qq.com>
2022-09-04 23:00:34 +0800

    cross_connect_socket test

--- src/CSocket.cpp
+++ src/CSocket.cpp
@@ -55,7 +55,19 @@ c_socket_t cross_open_socket(int domain, int type, int protocol, char* errorMsg)
     c_socket_t socket = INVALID_SOCKET;
     socket = c_socket(domain, type, protocol);
     if (socket == INVALID_SOCKET) {
+        perror("Invalid socket.\n");
     }
 
     return socket;
+}
+
+// TODO
+status_t cross_connect_socket(c_socket_t socket, const struct sockaddr* addr, socklen_t addrlen, char* errorMsg) {
+    status_t status = C_ERROR;
+    status = c_connect(socket, addr, addrlen);
+    if (status == C_ERROR) {
+        perror("Invalid socket.\n");
+    }
+
+    return status;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,19 @@
 #include <gtest/gtest.h>
 
+#include "../src/core.h"
 #include "../src/CSocket.h"
 
 TEST(SocketTest, BasicAssertions) {
     char errorMsg[512] = {0};
-    c_socket_t socket = cross_open_socket(AF_INET, SOCK_STREAM, 0, errorMsg);
+    c_socket_t socket = cross_open_socket(AF_INET, SOCK_STREAM, IPPROTO_TCP, errorMsg);
     EXPECT_NE(socket, INVALID_SOCKET);
+
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    socklen_t sockLen = sizeof(serv_addr);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    serv_addr.sin_port = htons(10000);
+    status_t status = cross_connect_socket(socket, (struct sockaddr*)&serv_addr, sockLen, errorMsg);
+    EXPECT_NE(status, C_ERROR);
 }
